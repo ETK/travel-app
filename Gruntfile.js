b40c56c2725f0575d16a7f47fc6de83d01bb2704
Kunal Mathur <kunal.mathur@tcs.com>
2016-02-16 17:22:39 +0530

    Adding initial Project Structure

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  eslint: {
+    target: ['scripts/**/*.js']
+  },  
+  wiredep: {
+    task: {
+      // Point to the files that should be updated when
+      // you run `grunt wiredep`
+      src: ['index.html']
+      }
+  },
+  csslint: {
+    strict: {
+      options: {
+        import: 2
+      },
+      src: ['styles/**/*.css']
+    }
+  },
+  karma: {
+    unit: {
+      configFile: 'karma.conf.js'
+    }
+  },
+  });
+
+  // Load all tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Default task(s).
+  grunt.registerTask('default', ['eslint']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# travel-app
+# Travel App
+An App which will allow users to get the best deals online on flights
+
+
+## Getting started
+1. Clone the project
+2. run npm install
+3. run bower install
+4. grunt wiredep	
